39f7a4dce491fd6dc25b1f09ec8f59ef4462f73c
youngjaechoi1977 <youngjae.choi1977@gmail.com>
2020-07-24 11:24:30 +0900

    텍스트 수정했음

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, STA for thaery first time");
+        System.out.println("Hello, STA for thaery first time!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
 }
