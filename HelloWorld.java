e7241185c3cdcf5930f64893cb9b384c822cbe0f
youngjaechoi1977 <youngjae.choi1977@gmail.com>
2020-07-24 14:16:03 +0900

    testesttest

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,23 @@
+import java.io.IOException;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello,  first time!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+     // 최상위 추가할 예외처리 라이브러리
+
+
+        // 메인창 안에다가 넣기
+        Runtime runtime = Runtime.getRuntime();
+
+        try{
+        	
+        	runtime.exec("C:\Program Files\Internet Explorer\iexplore.exe http://www.naver.com");
+
+        } catch(IOException ex) {
+
+        }
+
     }
 
 }
